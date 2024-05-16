6967bbe9df85a741891213168a514e2a25c5e9ec
Ahmed Refaat Mohamed <ahmedrefaat532@aucegypt.edu>
2024-05-15 17:08:08 -0700

    MR kind of works
    Still need to make it distribute keys

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,56 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+)
 
+type InputFile struct {
+	file      string
+	processed int
+}
 
 type Coordinator struct {
 	// Your definitions here.
-
+	InputFiles []InputFile
+	nReduce    int
 }
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
+func (c *Coordinator) AskForFile(args *AskForFileArgs, reply *AskForFileReply) error {
+	for i, f := range c.InputFiles {
+		if f.processed == 0 {
+			reply.Name = f.file
+			reply.State = 0
+			c.InputFiles[i].processed = 1
+			return nil
+		}
+	}
 
-//
+	for i, f := range c.InputFiles {
+		if f.processed == 1 {
+			reply.Name = "reducer"
+			reply.State = 1
+			c.InputFiles[i].processed = 2
+			return nil
+		}
+	}
+	return nil
+}
+
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +64,33 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
 
-
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
+	i := []InputFile{}
+	for _, f := range files {
+		i = append(i, InputFile{
+			file:      f,
+			processed: 0,
+		})
+	}
+	c := Coordinator{
+		InputFiles: i,
+		nReduce:    nReduce,
+	}
 
 	// Your code here.
-
 
 	c.server()
 	return &c
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,48 +1,124 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"log"
+	"net/rpc"
+	"os"
+	"sort"
+)
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
+// for sorting by key.
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
-func Worker(mapf func(string, string) []KeyValue,
+func Worker(
+	mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
 
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
+	for {
+		filename, state := AskForFile()
+		if filename == "" {
+			break
+		}
+		switch state {
+		case 0:
+			mapWorker(filename, mapf)
+		case 1:
+			reduceWorker(reducef)
+		case 2:
+			println("done")
+		default:
+			log.Fatal("what?")
+		}
+	}
 
 }
 
-//
+func reduceWorker(reducef func(string, []string) string) {
+	filenames, err := os.ReadDir(".")
+	if err != nil {
+		log.Fatal("cannot ls dir")
+	}
+	var intermediateFiles []string
+	for _, f := range filenames {
+		if f.Name()[:9] == "mr-inter-" {
+			intermediateFiles = append(intermediateFiles, f.Name())
+		}
+	}
+
+	var allKva [][]KeyValue
+	for _, f := range intermediateFiles {
+		file, err := os.Open(f)
+		if err != nil {
+			log.Fatal("Cannot open intermediate file in reduce")
+		}
+		dec := json.NewDecoder(file)
+		allKva = append(allKva, []KeyValue{})
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				log.Fatal("Cannot read intermediate kv in reduce")
+			}
+			allKva[len(allKva)-1] = append(allKva[len(allKva)-1], kv)
+		}
+	}
+	for {
+
+	}
+}
+
+func mapWorker(filename string, mapf func(string, string) []KeyValue) {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("cannot read %v", filename)
+	}
+	kva := mapf(filename, string(content))
+	sort.Sort(ByKey(kva))
+
+	oname := fmt.Sprintf("mr-inter-%s", filename[3:])
+	ofile, err := os.Create(oname)
+	if err != nil {
+		log.Fatalf("cannot create %v", oname)
+	}
+	defer ofile.Close()
+	enc := json.NewEncoder(ofile)
+	for _, kv := range kva {
+		err := enc.Encode(&kv)
+		if err != nil {
+			log.Fatalf("cannot write key val %v", filename)
+		}
+	}
+}
+
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -67,11 +143,21 @@ func CallExample() {
 	}
 }
 
-//
+func AskForFile() (string, int) {
+	args := AskForFileArgs{}
+	reply := AskForFileReply{}
+	ok := call("Coordinator.AskForFile", &args, &reply)
+	if ok {
+		return reply.Name, reply.State
+	} else {
+		fmt.Printf("call failed!\n")
+		panic("bad file")
+	}
+}
+
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
